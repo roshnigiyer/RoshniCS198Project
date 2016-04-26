9f0581b6615b16e04b1d03886af33c4782dfceb2
Morgan Rae <mreschenberg@berkeley.edu>
2016-04-25 17:58:56 -0700

    added tech page

--- Extension/V0.2/popup.js
+++ Extension/V0.2/popup.js
@@ -5,26 +5,4 @@
 // Store CSS data in the "local" storage area.
 //
 // See note in options.js for rationale on why not to use "sync".
-var storage = chrome.storage.local;
-
-var message = document.querySelector('#message');
-
-// Check if there is CSS specified.
-storage.get('css', function(items) {
-  console.log(items);
-  // If there is CSS specified, inject it into the page.
-  if (items.css) {
-    chrome.tabs.insertCSS({code: items.css}, function() {
-      if (chrome.runtime.lastError) {
-        message.innerText = 'Sorry, Chat is not supported for this page.';
-      } else {
-        message.innerText = 'Started Chat!';
-      }
-    });
-  } else {
-    var optionsUrl = chrome.extension.getURL('options.html');
-    message.innerHTML = 'Sign up/in on the <a target="_blank" href="' +
-        optionsUrl + '">options page</a> first.';
-  }
-});
 
